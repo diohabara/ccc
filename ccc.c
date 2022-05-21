08dfc1e8767fbe72f21c6af3846814c6ba79f76c
TKadoi <diohabara@gmail.com>
2022-05-21 18:12:10 +0900

    feat(step2): introduce addtion and subtraction
      - add related test cases

--- ccc.c
+++ ccc.c
@@ -7,10 +7,29 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("\tmov rax, %d\n", atoi(argv[1]));
+  printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "This character is unexpected: '%c'\n", *p);
+    return 1;
+  }
   printf("\tret\n");
   return 0;
 }
