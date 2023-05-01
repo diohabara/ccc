1790781575da9bab8f06d173078e02b6ff7b5481
卍diohabara卍 <diohabara@gmail.com>
2023-05-01 00:15:50 -0500

    feat: add -> operator

--- parse.c
+++ parse.c
@@ -492,7 +492,7 @@ Node *unary() {
   return postfix();
 }
 
-// postfix = primary ("[" expr "]" | "." ident)*
+// postfix = primary ("[" expr "]" | "." ident | "->" ident)*
 Node *postfix() {
   Node *node = primary();
   Token *tok;
@@ -506,6 +506,13 @@ Node *postfix() {
       continue;
     }
     if (tok = consume(".")) {
+      node = new_unary(ND_MEMBER, node, tok);
+      node->member_name = expect_ident();
+      continue;
+    }
+    if (tok = consume("->")) {
+      // x->y is short for (*x).y
+      node = new_unary(ND_DEREF, node, tok);
       node = new_unary(ND_MEMBER, node, tok);
       node->member_name = expect_ident();
       continue;
